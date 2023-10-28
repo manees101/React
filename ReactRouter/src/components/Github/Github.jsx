46a01aaf8f43569e4c5d1ef669d0f8185573e293
manees101 <muhammadaneesrajpoot@gmail.com>
2023-10-28 14:23:28 +0500

    React router project

---
+++ ReactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+function Github()
+{
+    const data=useLoaderData();
+    return (
+        <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl flex items-center flex-col gap-5">
+        Github Followers: {data.followers}
+
+        <img src={data.avatar_url} alt="Love Babbar" className="w-30 h-30 "/>
+        </div>
+    )
+}
+export default Github;
+export const getUserInfo=async()=>{
+  const result=await  fetch("https://api.github.com/users/lovebabbar")
+   return result;
+}
