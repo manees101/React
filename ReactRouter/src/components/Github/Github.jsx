0416d0a7d9325bc48039bd9bc17e8c14bc7c9d05
manees101 <muhammadaneesrajpoot@gmail.com>
2023-10-28 14:27:01 +0500

    React router project

--- ReactRouter/src/components/Github/Github.jsx
+++ ReactRouter/src/components/Github/Github.jsx
@@ -5,6 +5,8 @@ function Github()
     const data=useLoaderData();
     return (
         <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl flex items-center flex-col gap-5">
+            Name: {data.name}
+            <br />
         Github Followers: {data.followers}
 
         <img src={data.avatar_url} alt="Love Babbar" className="w-30 h-30 "/>
